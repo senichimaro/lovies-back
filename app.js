0b4ddade8d9eea5a072ed3dee648e9f66aa811ee
senichimaro <senichimaro@gmail.com>
2021-10-04 12:05:45 +0200

    allowing cors 4

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const endpoint = require('./routes/routes')
 const app = express()
+// const options = cors.CorsOptions = {
+//     origin:['*','https://senichimaro.github.io/lovies/', 'https://senichimaro.github.io/*', 'https://github.io/*']
+// }
+// app.use( cors( options ) )
+app.use( cors() )
 
 app.use( bodyParser.urlencoded({extended:false}) )
 app.use( bodyParser.json() )
 
-const options = cors.CorsOptions = {
-    origin:['*','https://senichimaro.github.io/lovies/', 'https://senichimaro.github.io/*', 'https://github.io/*']
-}
-app.use( cors( options ) )
 
 app.use( endpoint )
 
