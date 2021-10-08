4a882e86b6b022cea1bc21b63a419ed040fb3c2c
senichimaro <senichimaro@gmail.com>
2021-10-08 21:31:17 +0200

    cors headers

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,12 @@ const express = require('express')
 const endpoint = express.Router()
 const { getRoot, findCollection, addFavorites } = require('../controllers/controllers')
 
+endpoint.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "https://senichimaro.github.io/lovies/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 endpoint.get('/', getRoot )
 
 endpoint.post('/find-favorites', findCollection )
